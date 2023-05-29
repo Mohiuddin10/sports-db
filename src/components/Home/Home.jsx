2a8a659d7608c05f36dcd8b9da6b55f53b351f13
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-05-29 09:06:17 +0600

    working on search menu

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className="home-players">
+                <input type="text" name="" id="" />
+                <button className='search-btn'>Search</button>
+            </div>
+            <div className="home-cart">
+                <h1>Cart</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.css';
+
+const Nav = () => {
+    return (
+        <div className='nav'>
+            <h1>Hello from nav</h1>
+        </div>
+    );
+};
+
+export default Nav;
